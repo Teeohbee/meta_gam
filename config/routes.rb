30272afdc5b5a8022e81ab074e8aec2e8d7b3096
Toby Clarke <teeohbee@gmail.com>
2021-02-07 13:38:31 +0000

    Add top_ten to Pages controller and define route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/top_ten', to: 'pages#top_ten'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
