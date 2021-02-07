8c529b478c9554dac241a09c212038e3afdcbbe0
Toby Clarke <teeohbee@gmail.com>
2021-02-07 13:51:27 +0000

    Add httparty and byebug gems, fetch games from opencritic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
   end
 
   def top_ten
+    response = HTTParty.get('https://api.opencritic.com/api/game?platforms=xb1&sort=score')
+    @top_ten_games = response[0..9].map { |game| game['name'] }
   end
 end
