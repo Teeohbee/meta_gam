97e6d378be158479a30876952dc5b7f81f207ffb
Toby Clarke <teeohbee@gmail.com>
2021-02-07 14:11:44 +0000

    Display banner images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
 
   def top_ten
     response = HTTParty.get('https://api.opencritic.com/api/game?platforms=xb1&sort=score')
-    @top_ten_games = response[0..9].map { |game| game['name'] }
+    @top_ten_games = response[0..9].map { |game| OpenStruct.new(name: game['name'], img: 'https:' + game["bannerScreenshot"]["fullRes"]) }
   end
 end
